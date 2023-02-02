d492be16aeaf747cd37a960562e7ed04f76a66be
Marine Bascunana <marine.bascunana@gmail.com>
2023-02-02 18:41:26 +0100

    1ere validations ok + modification dans .gitignore >> Back-End VS Back_End

--- Back-End/src/auth/auth.controller.ts
+++ Back-End/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
