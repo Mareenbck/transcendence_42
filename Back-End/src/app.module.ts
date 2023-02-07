d5a0f4c2bddb8a06c9bfabe5975d865a9ee8e227
Theodore JUDLIN <theodore.judlin@free.fr>
2023-02-07 10:05:55 +0100

    chat

--- Back-End/src/app.module.ts
+++ Back-End/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
+  providers: [ChatGateway],
 })
 export class AppModule {}
