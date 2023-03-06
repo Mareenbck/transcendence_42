ed14fd92aac6f66feaa6443dd062c879655ac1d6
Mareenbck <marine.bascunana@gmail.com>
2023-03-06 10:10:06 +0100

    petite modification de route

--- Front-End/src/components/auth/SideBar.tsx
+++ Front-End/src/components/auth/SideBar.tsx
@@ -18,10 +18,10 @@ function SideBar(props: any)
                    <br/>
             <Link to="/game/play" className={style.btn}>Play Game </Link>
 				    <Link to="/chat/message" className={style.btn}>Chat</Link>
-				    
+
             <Link to={`/users/profile/${authCtx.userId}`} className={style.btn}>Profile page</Link>
-				    <Link to="../../pages/setting" className={style.btn}>setting</Link>
-				    <Link to="/auth/signin" className={style.btn}>Score</Link>    				
+				    <Link to="/settings" className={style.btn}>setting</Link>
+				    <Link to="/auth/signin" className={style.btn}>Score</Link>
 			</div>
     )
 
