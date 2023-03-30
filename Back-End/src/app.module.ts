a5d9e527847d82b2ef88e35b62eac3028d285705
Mariya SOSNOVA <msosnova@e1r1p8.clusters.42paris.fr>
2023-03-30 17:13:42 +0200

    29_03

--- Back-End/src/app.module.ts
+++ Back-End/src/app.module.ts
@@ -8,7 +8,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { ChatMessModule } from './chat/chat-mess/chat-mess.module';
 import { DirMessModule } from './chat/dir-mess/dir-mess.module';
 import { ChatroomModule } from './chat/chatroom2/chatroom2.module';
-import { JeuxModule } from './jeux/jeux.module';
 import { GameModule } from './game/game.module';
 //import { GameGateway } from './game/game.gateway';
 import { FriendshipModule } from './friendship/friendship.module';
@@ -27,7 +26,6 @@ import { FriendshipModule } from './friendship/friendship.module';
 		ChatroomModule,
 		ChatMessModule,
 		DirMessModule,
-		JeuxModule,
 		GameModule,
 
 		// PassportModule.register({ defaultStrategy: '42' }),
