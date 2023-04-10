206b333b1bd717631e72b9fd682969eb8801669d
Theodore JUDLIN <theodore.judlin@free.fr>
2023-04-10 18:58:21 +0200

    global BE

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -22,6 +22,7 @@ function App() {
 	const authCtx = useContext(AuthContext);
 	const [isLoggedIn, setLoggedIn] = useState(authCtx.isLoggedIn)
 	const [sendMessage, addListener] = useSocket();
+
 	useEffect(() => {
 		console.log({isLoggedIn})
 		if (isLoggedIn) sendMessage('welcome', 'welcome')
