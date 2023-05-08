4b544e3eecef00e70767a33d61cb731171205466
Saray JAFARZADEH <sjafarza@e1r2p7.clusters.42paris.fr>
2023-05-08 09:42:33 +0200

    bar scores

--- Front-End/src/components/scores/UserChart.tsx
+++ Front-End/src/components/scores/UserChart.tsx
@@ -7,11 +7,12 @@ import AuthContext from "../../store/AuthContext";
 const UserChart= (props: any) => {
 
     // const h = (props.h) * (100) / 3
-    const h = (props.h) * 20
+    // const h = (props.h) * 20
 
     return (
         <div className="combPos">
-            <div className="cadre" style={{height: h}}>
+            {/* <div className="cadre" style={{height: h}}> */}
+             <div className="cadre" >
                
                 <p>{props.userName} </p>
                 <p>{props.h}</p>
