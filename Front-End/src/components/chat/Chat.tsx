8218032dd4dd55171a8a990bd85f96e50ff5cf66
Theodore JUDLIN <theodore.judlin@free.fr>
2023-05-11 22:17:00 +0200

    1-update-participants-quand-join-private-2-participants-après-leave

--- Front-End/src/components/chat/Chat.tsx
+++ Front-End/src/components/chat/Chat.tsx
@@ -304,7 +304,7 @@ function Chat(props: any) {
 	useEffect(() => {
 	  addListener("showUsersList", data => setUsersList(data));
 	  if (!showUsersOnChannel) {
-		setShowUserList(true);
+		  setShowUserList(true);
 	  }
 	}, [showUsersOnChannel]);
 
@@ -313,6 +313,14 @@ function Chat(props: any) {
 		setActiveTab(newValue);
 	  };
 
+  // TESTS
+	useEffect(() => { 
+  if (currentChat)
+    {
+      setShowUserList(false);
+      setShowUsersOnChannel(true);
+    };
+  }, [currentChat]);
 
 	return (
 	  <>
