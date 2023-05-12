791aced41ec235d2b36f9522bfd8d9a3a34bbc4d
Mareenbck <marine.bascunana@gmail.com>
2023-05-12 15:29:34 +0200

    avatar des msg channel

--- Front-End/src/components/chat/message/messageD.tsx
+++ Front-End/src/components/chat/message/messageD.tsx
@@ -2,7 +2,7 @@ import './message.css'
 import {format} from 'timeago.js'
 import MyAvatar from '../../user/Avatar';
 import React from 'react';
-import { Avatar, ListItem, Tooltip } from '@mui/material';
+import { Avatar } from '@mui/material';
 
 export default function MessageD({ messageD, own, user, authCtx }) {
 
